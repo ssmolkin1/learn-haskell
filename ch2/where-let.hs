0a7693e8cc9483fee221ba7ca23e01fc6164eb69
Sam Smolkin <sam@future-precedent.org>
2018-01-13 22:36:24 +0000

    quick gal commit

---
+++ ch2/where-let.hs
@@ -0,0 +1,28 @@
+f1 :: Integer
+f1 = x * 3 + y
+   where x = 3
+         y = 4
+
+f2 :: Integer
+f2 = x * 5
+    where y = 10
+          x = 10 * 5 + y
+
+f3 :: Double
+f3 = z / x + y
+    where x = 7
+          y = negate x
+          z = y * 10
+
+waxOn ::  Integer
+waxOn = x * 5
+    where z = 7
+          x = y ^ 2
+          y = z + 8
+
+triple :: Num a => a -> a
+triple x = x * 3
+
+waxOff :: Num a => a -> a
+waxOff x = triple x ^ 2
+    where x = x + 1
